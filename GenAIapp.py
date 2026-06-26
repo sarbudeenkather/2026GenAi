b533b6b408895c2f77016fac7eea5848d7dde922
Sarbudeen Kather <sarbudeenkather7@gmail.com>
2026-06-26 14:55:45 +0530

    Update GenAIapp.py

--- GenAIapp.py
+++ GenAIapp.py
@@ -2,11 +2,11 @@ import streamlit as st
 from google import genai
 from google.genai import types
 config = types.GenerateContentConfig(
-        system_instruction = ".You are an expert Python developer.\
-  Answer only questions related to Python programming.\
-  For any non-Python question, reply exactly:\
-  Please ask a Python-related question.\
-  Do not answer questions outside the Python domain."
+        system_instruction = """.You are an expert Python developer.
+  Answer only questions related to Python programming.
+  For any non-Python question, reply exactly:
+  Please ask a Python-related question.
+  Do not answer questions outside the Python domain."""
     )
 st.markdown(
     """
@@ -17,7 +17,7 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-client = genai.Client( api_key=st.secrets["GOOGLE_API_KEY"])
+robo = genai.Client( api_key=st.secrets["GOOGLE_API_KEY"])
 mychat = robo.chats.create(model="gemini-flash-lite-latest")
 # Placeholder for the response
 response_placeholder = st.empty()
