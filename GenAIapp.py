bd27dbf59bd92452ef99571f2b229581ca72440c
Sarbudeen Kather <sarbudeenkather7@gmail.com>
2026-06-26 14:32:46 +0530

    Create GenAIapp.py

---
+++ GenAIapp.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from google import genai
+from google.genai import types
+st.markdown(
+    """
+    <h1 style='text-align: center;'> Python AI Assistant</h1>
+    <p style='text-align: center; font-size:18px;'>
+        Ask any Python programming question.
+    </p>
+    """,
+    unsafe_allow_html=True,
+)
+config = types.GenerateContentConfig(
+        system_instruction = ".You are an expert Python developer.\
+  Answer only questions related to Python programming.\
+  For any non-Python question, reply exactly:\
+  Please ask a Python-related question.\
+  Do not answer questions outside the Python domain."
+    )
+import streamlit as st
+from google import genai
+st.markdown(
+    """
+    <h1 style='text-align: center;'> Python AI Assistant</h1>
+    <p style='text-align: center; font-size:18px;'>
+        Ask any Python programming question.
+    </p>
+    """,
+    unsafe_allow_html=True,
+)
+client = genai.Client( api_key=st.secrets["GOOGLE_API_KEY"])
+mychat = robo.chats.create(model="gemini-flash-lite-latest")
+# Placeholder for the response
+response_placeholder = st.empty()
+
+question = st.text_input("", placeholder="Enter your Python question here...")
+
+col1, col2, col3 = st.columns([4, 1, 4])
+
+with col2:
+    send =st.button("Send")
+if send:
+    response = mychat.send_message(question)
+    response_placeholder.write(response.text)
+
+mychat = robo.chats.create(model="gemini-flash-lite-latest")
+#Placeholder for the response
+response_placeholder = st.empty()
+
+question = st.text_input("", placeholder="Enter your Python question here...")
+
+col1, col2, col3 = st.columns([4, 1, 4])
+
+with col2:
+    send =st.button("Send")
+
+if send:
+    question = question + config.system_instruction
+    response = mychat.send_message(question)
+    response_placeholder.write(response.text)
